6f5867fd6cda288a1fe15ffb45c1c83119772004
Lars Baldes <lars.baldes@wps.de>
2020-04-03 21:11:46 +0200

    added Property check for Entities

--- DDDAnalyzer/DDDAnalyzer.Test/DDDAnalyzerUnitTests.cs
+++ DDDAnalyzer/DDDAnalyzer.Test/DDDAnalyzerUnitTests.cs
@@ -21,59 +21,6 @@ namespace DDDAnalyzer.Test
             VerifyCSharpDiagnostic(test);
         }
 
-        //Diagnostic and CodeFix both triggered and checked for
-        [Test]
-        public void Test2()
-        {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TypeName
-        {   
-        }
-    }";
-            var expected = new DiagnosticResult
-            {
-                Id = "DDDAnalyzer",
-                Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-                Severity = DiagnosticSeverity.Warning,
-                Locations =
-                    new[] {
-                            new DiagnosticResultLocation("Test0.cs", 11, 15)
-                        }
-            };
-
-            VerifyCSharpDiagnostic(test, expected);
-
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
-        }
-    }";
-            VerifyCSharpFix(test, fixtest);
-        }
-
-        [Test]
-        public void UNIT_PRECONDITION_EXPECTATION()
-        {
-        }
-
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
             return new DDDAnalyzerCodeFixProvider();
--- DDDAnalyzer/DDDAnalyzer/ValueObjectAnalyzer.cs
+++ DDDAnalyzer/DDDAnalyzer/ValueObjectAnalyzer.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
 using DDDAnalyzer.Attributes;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace DDDAnalyzer
@@ -16,14 +11,12 @@ namespace DDDAnalyzer
     {
         public const string DiagnosticId = "DDDAnalyzer";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.ValueObjectAnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.ValueObjectAnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.ValueObjectAnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Design";
 
-        private static DiagnosticDescriptor ValueObjectMustNotUseEntityRule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+        private static readonly DiagnosticDescriptor ValueObjectMustNotUseEntityRule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(ValueObjectMustNotUseEntityRule); } }
 
@@ -34,7 +27,7 @@ namespace DDDAnalyzer
             context.RegisterSymbolAction(AnalyzeProperty, SymbolKind.Property);
         }
 
-        private void AnalyzeMethod(SymbolAnalysisContext context)
+        private static void AnalyzeMethod(SymbolAnalysisContext context)
         {
             var method = (IMethodSymbol)context.Symbol;
             var type = method.ContainingType;
@@ -45,18 +38,18 @@ namespace DDDAnalyzer
             }
         }
 
-        private void CheckThatReturnTypeIsNotEntity(SymbolAnalysisContext context, IMethodSymbol method)
+        private static void CheckThatReturnTypeIsNotEntity(SymbolAnalysisContext context, IMethodSymbol method)
         {
             if (!method.ReturnsVoid)
             {
                 if (IsEntity(method.ReturnType))
                 {
-                    EmitEntityViolation(context, method.ReturnType);
+                    EmitEntityViolation(context, method);
                 }
             }
         }
 
-        private void CheckThatParametersAreNotEntities(SymbolAnalysisContext context, IMethodSymbol method)
+        private static void CheckThatParametersAreNotEntities(SymbolAnalysisContext context, IMethodSymbol method)
         {
             foreach (var parameter in method.Parameters)
             {
@@ -68,23 +61,32 @@ namespace DDDAnalyzer
             }
         }
 
-        private static void EmitEntityViolation(SymbolAnalysisContext context, ISymbol parameter)
+        private static void EmitEntityViolation(SymbolAnalysisContext context, ISymbol symbol)
         {
-            var diagnostic = Diagnostic.Create(ValueObjectMustNotUseEntityRule, parameter.Locations[0]);
+            var diagnostic = Diagnostic.Create(ValueObjectMustNotUseEntityRule, symbol.Locations[0]);
             context.ReportDiagnostic(diagnostic);
         }
 
-        private void AnalyzeProperty(SymbolAnalysisContext context)
+        private static void AnalyzeProperty(SymbolAnalysisContext context)
         {
+            var propertySymbol = (IPropertySymbol)context.Symbol;
+            var classType = propertySymbol.ContainingType;
+            if (IsValueObject(classType))
+            {
+                if (IsEntity(propertySymbol.Type))
+                {
+                    EmitEntityViolation(context, propertySymbol);
+                }
+            }
         }
 
-        private bool IsEntity(ITypeSymbol parameterType)
+        private static bool IsEntity(ISymbol symbol)
         {
-            var attributes = parameterType.GetAttributes().ToArray();
+            var attributes = symbol.GetAttributes().ToArray();
             return attributes.Any(it => it.AttributeClass.Name.Equals(nameof(EntityAttribute)));
         }
 
-        private bool IsValueObject(INamedTypeSymbol type)
+        private static bool IsValueObject(INamedTypeSymbol type)
         {
             var attributes = type.GetAttributes();
             return attributes.Any(IsValueObject);
